9b1e893a8e3bca15274f41d82093434170e62584
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-23 12:23:01 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-4xl text-center text-cyan-300">Hello</h1>
+    </>
+  )
+}
+
+export default App
