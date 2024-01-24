1867b0abc51cafb157643afab3112f8e8f364f9d
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-24 18:57:12 +0530

    added redux

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
 
 const SignIn = () => {
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart()); 
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -25,18 +27,13 @@ const SignIn = () => {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null); 
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (err) {
-      console.log(err);
-      setLoading(false);
-      setError(`Something went wrong: ${err.message}`);
-
+      dispatch(signInFailure(err.message));
     }
   };
 
