d9410f1f8b973547290ccda92cdca6ded8cb216e
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-25 06:57:31 +0530

    added protected route to profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path ="/sign-up" element={<SignUp />}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center mx-auto p-3">
@@ -21,20 +24,26 @@ const Header = () => {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-            <Link to="/">
-          <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
-            Home
-          </li>
-            </Link>
-            <Link to="/about">
-          <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
-            About
-          </li>
-            </Link>
-            <Link to="/sign-in">
-          <li className="text-slate-700 hover:underline cursor-pointer">
-            Sign In
-          </li>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img 
+              className="rounded-full h-8 w-8 object-cover"
+              src= {currentUser.avatar} alt="profile" />
+            ) : (
+              <li className="text-slate-700 hover:underline cursor-pointer">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
