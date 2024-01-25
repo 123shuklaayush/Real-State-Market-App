b6fd252247193f3d26d8b5555e4ee4d1ce5d8460
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-25 09:11:34 +0530

    added profile UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,28 +1,67 @@
-import React from 'react'
-import { signOutUserFailure, signOutUserStart, signOutUserSuccess } from '../redux/user/userSlice'
-import { useDispatch } from 'react-redux'
+import React from "react";
+import {
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 const Profile = () => {
-  const dispatch = useDispatch()
-  const handleSignOut = async() => {
-    try{
-      dispatch(signOutUserStart())
-      const res = await fetch('/api/auth/signout')
-      const data = await res.json()
-      if(data.success === false){
-        dispatch(signOutUserFailure(data.message))
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
         return;
       }
-      dispatch(signOutUserSuccess(data))
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
     }
-    catch(error){
-      dispatch(signOutUserFailure(data.message))
-    }
-  }
+  };
   return (
-    <button 
-    onClick = {handleSignOut}
-    >Sign Out</button>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7"></h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
 
-export default Profile
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button 
+        className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >Update</button>
+        
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick = {handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
