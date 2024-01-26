a415afd003e24067330b6b7890672816da805738
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-26 19:06:24 +0530

    added delete listings to server

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -10,3 +10,23 @@ export const createListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only delete your own listings!'));
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            message: 'Listing deleted successfully!',
+        });
+    }
+    catch(error){
+        next(error);
+    }
+}
