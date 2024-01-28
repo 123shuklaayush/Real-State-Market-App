7a8e3eb6d90c7d73cf8bd8e82be6149726b19fb6
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-28 13:25:44 +0530

    added deployment prerequisites

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://real-estate-application-k9nm.onrender.com',
         secure: false,
       },
     },
--- server/index.js
+++ server/index.js
@@ -5,10 +5,16 @@ import userRoute from "./routes/user.route.js"
 import authRoute from "./routes/auth.route.js"
 import listingRoute from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -18,6 +24,8 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err) => {
   console.log(err);
 });
+
+const __dirname = path.resolve();
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
@@ -30,6 +38,12 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use('/api/listing', listingRoute);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
